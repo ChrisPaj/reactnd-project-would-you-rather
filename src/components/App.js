948a7ac39cc309c47b5bf793a084734091f34aa6
Christian Pajung <christian.pajung@telekom.de>
2019-01-25 12:36:33 +0100

    move listing-logic to Startpage

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './Header';
 import { handleInitialData } from '../actions/shared'
 import { connect } from 'react-redux'
+import Startpage from './Startpage'
 
 class App extends Component {
   componentDidMount(){
@@ -11,21 +12,10 @@ class App extends Component {
     return (
       <div className="App">
           <Header />
-          {this.props.questionIds.map((id) => (
-            <div key={id}>{id}</div>
-          ))}
+          <Startpage />
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
-  const { questions } = state
-  const questionIds = Object.keys(questions)
-  return({
-    questionIds: questionIds
-    .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
-  })
-}
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
